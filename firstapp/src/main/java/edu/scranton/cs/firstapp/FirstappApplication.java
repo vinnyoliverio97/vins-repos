11cde3b6d1aa9fabbb00555b81cdf98580169484
vinny <vinny@10.0.0.20>
2020-05-12 16:00:56 -0400

    idk

--- firstapp/src/main/java/edu/scranton/cs/firstapp/FirstappApplication.java
+++ firstapp/src/main/java/edu/scranton/cs/firstapp/FirstappApplication.java
@@ -1,10 +1,16 @@
 package edu.scranton.cs.firstapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstappApplication {
+public class FirstappApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FirstappApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstappApplication.class, args);
